84e39d175e5179a03cbf4979e687e1a2b19b875f
sidekick-eimantas <111279287+sidekick-eimantas@users.noreply.github.com>
2022-10-11 08:47:12 +0100

    Add 1password-cli cask (#7)

---
+++ Casks/1password-cli.rb
@@ -0,0 +1,23 @@
+cask "1password-cli" do
+  version "2.7.1"
+  sha256 "8fe7bdcb5150a77d0ee691552db6d786dadbc918d66593c586415f3570dc08fb"
+
+  url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_apple_universal_v#{version}.pkg",
+      verified: "cache.agilebits.com/dist/1P/op2/pkg/"
+  name "1Password CLI"
+  desc "Command-line helper for the 1Password password manager"
+  homepage "https://developer.1password.com/docs/cli"
+
+  livecheck do
+    url "https://app-updates.agilebits.com/product_history/CLI2"
+    regex(%r{href=.*?/op_apple_universal[._-]v?(\d+(?:\.\d+)+)\.pkg}i)
+  end
+
+  conflicts_with cask: "1password-cli1"
+
+  pkg "op_apple_universal_v#{version}.pkg"
+
+  uninstall pkgutil: "com.1password.op"
+
+  zap trash: "~/.op"
+end
