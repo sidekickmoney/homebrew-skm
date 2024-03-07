2d4b778924b5d466347217e06fe6aaab7fc20820
Andrew Debens <andrew.debens@sidekickmoney.com>
2024-03-07 09:36:43 +0000

    feat: upgrading node to 18.19.1

--- Formula/node.rb
+++ Formula/node.rb
@@ -1,34 +1,41 @@
 class Node < Formula
   desc "Platform built on V8 to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v18.11.0/node-v18.11.0.tar.xz"
-  sha256 "8b9643dc6fce79c1e99379db0ce64e43601e2e2d7389015fe8985cc4ccd0ce17"
+  url "https://nodejs.org/dist/v18.19.1/node-v18.19.1.tar.xz"
+  sha256 "090f96a2ecde080b6b382c6d642bca5d0be4702a78cb555be7bf02b20bd16ded"
   license "MIT"
-  head "https://github.com/nodejs/node.git", branch: "main"
+  revision 1
 
   livecheck do
     url "https://nodejs.org/dist/"
-    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
+    regex(%r{href=["']?v?(18(?:\.\d+)+)/?["' >]}i)
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "fd0da21de656cd319c3c478c1bb1ceaf7a6ec54791716e00617ff568aef30ad6"
-    sha256 cellar: :any,                 arm64_monterey: "2dae00520299dd77f3a5d15b16254d71759b3d5d5770627b06e9b072fdea8460"
-    sha256 cellar: :any,                 arm64_big_sur:  "27e7d698b613618bc8ea0a2034253917a643747b160f376a895c068551737305"
-    sha256 cellar: :any,                 monterey:       "7ee7361d52d8681048220760f55192210196a8e4301abccd0153cce56cc585af"
-    sha256 cellar: :any,                 big_sur:        "3c166868f057be76e82897fea7f4734e14dae34df7bdb309e0c68ceec558bd36"
-    sha256 cellar: :any,                 catalina:       "703916644c5401ef73dda2fd106c2d89f851f876685f651e8aa6c38ff25c8208"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e7002d0c460a1168d8ce31a9bacfe2325647b3bbba248b77e42f44527efc6bde"
+    sha256 arm64_sonoma:   "6ceb39cdb19984aed3125dff77678c77d136c40a492742a01d7aac6921e802ef"
+    sha256 arm64_ventura:  "3eb7bc97555f2c6bfd0336621dcdc95b19070ca9bc78e1cfdc3a39a71c32ca50"
+    sha256 arm64_monterey: "8073af2db5b500f9a6aa9d897103b41eb13f647fa6fff439e0c80dde0921700e"
+    sha256 sonoma:         "c5b3ccb0d88886acb6d7d0afb002ca0dd3fed6abacabd115956baf501517cb59"
+    sha256 ventura:        "7b6f43da3692969a73875f0c2b67b1812ffbe10331594ce31ce7c80b37825d61"
+    sha256 monterey:       "76e403212a3cf036325c835b6276d340ec88835b7bcdd59fd7e8e3a39546d16c"
+    sha256 x86_64_linux:   "2e3a2c258c64de94eae19f8c7c1eab88fe9ba5273cd53a3d405a7a8b09557104"
   end
 
+  keg_only :versioned_formula
+
+  # https://github.com/nodejs/release#release-schedule
+  # disable! date: "2025-04-30", because: :unsupported
+  deprecate! date: "2024-10-29", because: :unsupported
+
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.12" => :build
   depends_on "brotli"
   depends_on "c-ares"
   depends_on "icu4c"
   depends_on "libnghttp2"
   depends_on "libuv"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   uses_from_macos "python", since: :catalina
   uses_from_macos "zlib"
@@ -46,25 +53,17 @@ class Node < Formula
 
   fails_with gcc: "5"
 
-  # We track major/minor from upstream Node releases.
-  # We will accept *important* npm patch releases when necessary.
-  resource "npm" do
-    url "https://registry.npmjs.org/npm/-/npm-8.19.2.tgz"
-    sha256 "439fb2276f7039d2fba2739e361006f3bc25d6a6b3f88c1edb4d28ab5a7eb3f7"
-  end
+  # Support Python 3.12
+  patch :DATA
 
   def install
     ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1100)
 
     # make sure subprocesses spawned by make are using our Python 3
-    ENV["PYTHON"] = which("python3.10")
+    ENV["PYTHON"] = which("python3.12")
 
-    # Never install the bundled "npm", always prefer our
-    # installation from tarball for better packaging control.
     args = %W[
       --prefix=#{prefix}
-      --without-npm
-      --without-corepack
       --with-intl=system-icu
       --shared-libuv
       --shared-nghttp2
@@ -76,70 +75,21 @@ class Node < Formula
       --shared-libuv-libpath=#{Formula["libuv"].lib}
       --shared-nghttp2-includes=#{Formula["libnghttp2"].include}
       --shared-nghttp2-libpath=#{Formula["libnghttp2"].lib}
-      --shared-openssl-includes=#{Formula["openssl@1.1"].include}
-      --shared-openssl-libpath=#{Formula["openssl@1.1"].lib}
+      --shared-openssl-includes=#{Formula["openssl@3"].include}
+      --shared-openssl-libpath=#{Formula["openssl@3"].lib}
       --shared-brotli-includes=#{Formula["brotli"].include}
       --shared-brotli-libpath=#{Formula["brotli"].lib}
       --shared-cares-includes=#{Formula["c-ares"].include}
       --shared-cares-libpath=#{Formula["c-ares"].lib}
       --openssl-use-def-ca-store
     ]
-    args << "--tag=head" if build.head?
-
-    # Enabling LTO errors on Linux with:
-    # terminate called after throwing an instance of 'std::out_of_range'
-    # Pre-Catalina macOS also can't build with LTO
-    # LTO is unpleasant if you have to build from source.
-    args << "--enable-lto" if MacOS.version >= :catalina && build.bottle?
 
     system "./configure", *args
     system "make", "install"
-
-    # Allow npm to find Node before installation has completed.
-    ENV.prepend_path "PATH", bin
-
-    bootstrap = buildpath/"npm_bootstrap"
-    bootstrap.install resource("npm")
-    # These dirs must exists before npm install.
-    mkdir_p libexec/"lib"
-    system "node", bootstrap/"bin/npm-cli.js", "install", "-ddd", "--global",
-            "--prefix=#{libexec}", resource("npm").cached_download
-
-    # The `package.json` stores integrity information about the above passed
-    # in `cached_download` npm resource, which breaks `npm -g outdated npm`.
-    # This copies back over the vanilla `package.json` to fix this issue.
-    cp bootstrap/"package.json", libexec/"lib/node_modules/npm"
-    # These symlinks are never used & they've caused issues in the past.
-    rm_rf libexec/"share"
-
-    bash_completion.install bootstrap/"lib/utils/completion.sh" => "npm"
   end
 
   def post_install
-    node_modules = HOMEBREW_PREFIX/"lib/node_modules"
-    node_modules.mkpath
-    # Kill npm but preserve all other modules across node updates/upgrades.
-    rm_rf node_modules/"npm"
-
-    cp_r libexec/"lib/node_modules/npm", node_modules
-    # This symlink doesn't hop into homebrew_prefix/bin automatically so
-    # we make our own. This is a small consequence of our
-    # bottle-npm-and-retain-a-private-copy-in-libexec setup
-    # All other installs **do** symlink to homebrew_prefix/bin correctly.
-    # We ln rather than cp this because doing so mimics npm's normal install.
-    ln_sf node_modules/"npm/bin/npm-cli.js", HOMEBREW_PREFIX/"bin/npm"
-    ln_sf node_modules/"npm/bin/npx-cli.js", HOMEBREW_PREFIX/"bin/npx"
-
-    # Create manpage symlinks (or overwrite the old ones)
-    %w[man1 man5 man7].each do |man|
-      # Dirs must exist first: https://github.com/Homebrew/legacy-homebrew/issues/35969
-      mkdir_p HOMEBREW_PREFIX/"share/man/#{man}"
-      # still needed to migrate from copied file manpages to symlink manpages
-      rm_f Dir[HOMEBREW_PREFIX/"share/man/#{man}/{npm.,npm-,npmrc.,package.json.,npx.}*"]
-      ln_sf Dir[node_modules/"npm/man/#{man}/{npm,package-,shrinkwrap-,npx}*"], HOMEBREW_PREFIX/"share/man/#{man}"
-    end
-
-    (node_modules/"npm/npmrc").atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
+    (lib/"node_modules/npm/npmrc").atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
   end
 
   test do
@@ -161,13 +111,37 @@ class Node < Formula
     ENV.prepend_path "PATH", opt_bin
     ENV.delete "NVM_NODEJS_ORG_MIRROR"
     assert_equal which("node"), opt_bin/"node"
-    assert_predicate HOMEBREW_PREFIX/"bin/npm", :exist?, "npm must exist"
-    assert_predicate HOMEBREW_PREFIX/"bin/npm", :executable?, "npm must be executable"
+    assert_predicate bin/"npm", :exist?, "npm must exist"
+    assert_predicate bin/"npm", :executable?, "npm must be executable"
     npm_args = ["-ddd", "--cache=#{HOMEBREW_CACHE}/npm_cache", "--build-from-source"]
-    system HOMEBREW_PREFIX/"bin/npm", *npm_args, "install", "npm@latest"
-    system HOMEBREW_PREFIX/"bin/npm", *npm_args, "install", "ref-napi" unless head?
-    assert_predicate HOMEBREW_PREFIX/"bin/npx", :exist?, "npx must exist"
-    assert_predicate HOMEBREW_PREFIX/"bin/npx", :executable?, "npx must be executable"
-    assert_match "< hello >", shell_output("#{HOMEBREW_PREFIX}/bin/npx --yes cowsay hello")
+    system bin/"npm", *npm_args, "install", "npm@latest"
+    system bin/"npm", *npm_args, "install", "ref-napi" unless head?
+    assert_predicate bin/"npx", :exist?, "npx must exist"
+    assert_predicate bin/"npx", :executable?, "npx must be executable"
+    assert_match "< hello >", shell_output("#{bin}/npx --yes cowsay hello")
   end
 end
+
+
+__END__
+diff --git a/configure b/configure
+index fefb313c..711a3014 100755
+--- a/configure
++++ b/configure
+@@ -4,6 +4,7 @@
+ # Note that the mix of single and double quotes is intentional,
+ # as is the fact that the ] goes on a new line.
+ _=[ 'exec' '/bin/sh' '-c' '''
++command -v python3.12 >/dev/null && exec python3.12 "$0" "$@"
+ command -v python3.11 >/dev/null && exec python3.11 "$0" "$@"
+ command -v python3.10 >/dev/null && exec python3.10 "$0" "$@"
+ command -v python3.9 >/dev/null && exec python3.9 "$0" "$@"
+@@ -23,7 +24,7 @@ except ImportError:
+   from distutils.spawn import find_executable as which
+
+ print('Node.js configure: Found Python {}.{}.{}...'.format(*sys.version_info))
+-acceptable_pythons = ((3, 11), (3, 10), (3, 9), (3, 8), (3, 7), (3, 6))
++acceptable_pythons = ((3, 12), (3, 11), (3, 10), (3, 9), (3, 8), (3, 7), (3, 6))
+ if sys.version_info[:2] in acceptable_pythons:
+   import configure
+ else:
