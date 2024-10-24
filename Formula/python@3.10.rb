6089f6b4498e318ba53db91da09d27f34e3e4378
sidekick-rob <161023990+sidekick-rob@users.noreply.github.com>
2024-10-24 10:01:30 +0100

    Update python@3.10.rb
    Change the dependency on openssl@1.1 to openssl@3
    
    Homebrew no longer supports openssl@1.1, it was EOLed upstream in 2023

--- Formula/python@3.10.rb
+++ Formula/python@3.10.rb
@@ -26,7 +26,7 @@ class PythonAT310 < Formula
   depends_on "pkg-config" => :build
   depends_on "gdbm"
   depends_on "mpdecimal"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "readline"
   depends_on "sqlite"
   depends_on "xz"
